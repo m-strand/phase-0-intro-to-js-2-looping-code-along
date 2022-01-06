5ce615d3ab8865be21cbae551a18dc9371bcbafc
Brooke Strand <brooke.strand007@gmail.com>
2022-01-06 01:10:29 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = []; 
+function writeCards(names, holiday) { 
+    for (let i=0; i < names.length; ++i) { 
+        let note = "Thank you, " + names[i] + ", for the wonderful " + holiday + " gift!"; 
+        messages.push(note);
+    }
+    return messages;
+}
+
+function countDown(i) {
+    let startPoint = i; 
+    while(i >= 0) {
+        console.log(i);
+        --i;
+    }
+}
